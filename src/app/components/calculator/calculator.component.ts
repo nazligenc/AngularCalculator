71e5184a74baea0e78c8d403ed3e71e57c5bd090
nazligenc <nazli.gencx@gmail.com>
2024-07-08 17:00:25 +0300

    update

---
+++ src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+import { NgClass, NgForOf, NgIf } from "@angular/common";
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf,
+    NgClass
+  ],
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+  display = '0';
+  result = '';
+  isDarkMode = false;
+  showHistory = false;
+  lastThreeOperations: string[] = [];
+
+  toggleHistory() {
+    this.showHistory = !this.showHistory;
+  }
+
+  addOperationToHistory(operation: string) {
+    this.lastThreeOperations.unshift(operation);
+    if (this.lastThreeOperations.length > 3) {
+      this.lastThreeOperations.pop();
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
+  calculate() {
+    try {
+      this.result = this.safeEval(this.display.replace(/×/g, '*').replace(/÷/g, '/')).toString();
+      this.addOperationToHistory(this.display + ' = ' + this.result);
+    } catch (e) {
+      this.result = 'Error';
+    }
+  }
+
+  appendNumber(num: number) {
+    if (this.display === '0' || this.display === 'Error') {
+      this.display = String(num);
+    } else {
+      this.display += String(num);
+    }
+  }
+
+  appendOperator(operator: string) {
+    const lastChar = this.display[this.display.length - 1];
+    const operators = ['+', '-', '×', '÷'];
+
+    if (operators.includes(lastChar)) {
+      return;
+    }
+
+    if (operator === '.') {
+      const parts = this.display.split(/[\+\-\×\÷]/);
+      const currentPart = parts[parts.length - 1];
+      if (currentPart.includes('.')) {
+        return;
+      }
+    }
+
+    this.display += operator;
+  }
+
+  clear() {
+    this.display = '0';
+    this.result = '';
+  }
+
+  toggleSign() {
+    if (this.display === '0') {
+      return;
+    } else if (this.display.startsWith('-')) {
+      this.display = this.display.substring(1);
+    } else {
+      this.display = '-' + this.display;
+    }
+  }
+
+  appendZeroes(num: number) {
+    if (this.display !== '0') {
+      this.display += '00';
+    }
+  }
+
+  safeEval(expression: string): number {
+    try {
+      const result = Function('"use strict"; return (' + expression + ')')();
+      return Math.round((result + Number.EPSILON) * 1000000000) / 1000000000;
+    } catch (e) {
+      return NaN;
+    }
+  }
+}
